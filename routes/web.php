7ee28889d11a059e3fbb6d91c097991ac9034eb7
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2024-06-01 23:36:50 -0300

    Adicionado página inicial para mostrar que está funcionando

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function() {
+    return 'This will be an API';
+});
+
 Route::any('/ping', function (Request $request) {
     return [
         'pong' => true,
